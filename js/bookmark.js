d7079d585e6dc3f28d412df8e0b64b681a230382
Anna Sandoval <art_90@web.de>
2022-06-10 22:35:56 +0200

    change design

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,10 +1,10 @@
-// export { toggleBookmark1, toggleBookmark2, toggleBookmark3 };
-
-export default function toggleBookmark(){
-    const bookmarks = document.querySelectorAll("[data-js=bookmark]")
-    bookmarks.forEach((bookmark) => {
-bookmark.addEventListener("click", () => {
-    bookmark.classList.toggle("card__bookmark--active")
-} )
-    ))
+export default function toggleBookmark() {
+  const bookmarks = document.querySelectorAll("[data-js=bookmarks]");
+  console.log(bookmarks);
+  bookmarks.forEach((bookmark) => {
+    bookmark.addEventListener("click", () => {
+      bookmark.classList.toggle("bookmark--marked");
+      console.log("click");
+    });
+  });
 }
